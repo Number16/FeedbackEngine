5b2863a1e81919383201b11bd9a5e5a4dc2cf984
lek <leks@lekskeks.com>
2022-09-13 13:44:17 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# FeedbackEngine
+
+A description of this package.
---
+++ Sources/FeedbackEngine/FeedbackEngine.swift
@@ -0,0 +1,6 @@
+public struct FeedbackEngine {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
